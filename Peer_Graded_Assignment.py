d7cc59892025958558f1be58c2d7cb5d76f3e504
Rose Nyaga <119519946+RoseMumbi@users.noreply.github.com>
2023-01-10 16:20:37 +0300

    Update Peer_Graded_Assignment.py

--- Peer_Graded_Assignment.py
+++ Peer_Graded_Assignment.py
@@ -75,7 +75,7 @@ app.layout = html.Div(children=[
                                 # TASK1: Add title to the dashboard
                                 # Enter your code below. Make sure you have correct formatting.
                                  html.H1('US Domestic Airline Flights Performance',
-                                 style={'testAlign':'center','color':'#503D36','font-size':24}
+                                 style={'textAlign':'center','color':'#503D36','font-size':24}
                                  ),
                                 # REVIEW2: Dropdown creation
                                 # Create an outer division 
@@ -95,7 +95,7 @@ app.layout = html.Div(children=[
                                                           {'label':'Yearly Airline Performance Report', 'value':'OPT1'},
                                                           {'label':'Yearly Airline Delay Report', 'value':'OPT2'}],
                                                           placeholder='Select a report type',
-                                                          style={'width':'80%','padding':'3px','fonr-size':'20px','test-align-last':'center'}
+                                                          style={'width':'80%','padding':'3px','font-size':'20px','text-align-last':'center'}
                                                     )
                                     # Place them next to each other using the division style
                                     ], style={'display':'flex'}),
